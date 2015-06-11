3016c69f3bac9b2f04c33195f326c69c6317dc51
Doyi Lee <doyilee@hotmail.com>
2015-06-11 15:30:16 -0600

    added user authentication to user_controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_admin
+
+  def current_user
+  	@current_user ||= User.where(id: session[:user_id]).first
+  end
+
+  private
+
+  def authenticate_admin
+  	unless session[:user_id].present? && current_user.present?
+  		redirect_to(login_path, notice: 'Please login to start Dating.')
+  	end
+  end
 end

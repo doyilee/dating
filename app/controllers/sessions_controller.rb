d90aedb5cf54eef0332a56860c1ddd1639f16210
Doyi Lee <doyilee@hotmail.com>
2015-06-11 15:56:09 -0600

    added login logic and path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	
+  	@user = User.where(username: params[:username]).first
+  	if @user.present? && @user.authenticate(params[:password])
+  		session[:user_id] = @user.id
+  		flash[:notice] = "You have logged in."
+  		redirect_to ''
+  	else
+  		flash[:alert] = "Failed login. Try again."
+  		render :new
+  	end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
   	params.require(:user).permit(:gender, :age, :name, :username, :password, :password_confirmation)
   end
